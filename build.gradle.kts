9e0c7878f3ed4637da8e9165b3657776b2b939c4
José Luis González <joseluisgonsan@gmail.com>
2023-05-02 19:11:37 +0200

    add logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
+// Logger
+val micrologging_version: String by project
+val logbackclassic_version: String by project
+
 plugins {
     kotlin("jvm") version "1.8.21"
     id("io.ktor.plugin") version "2.3.0"
@@ -22,11 +26,20 @@ repositories {
 }
 
 dependencies {
+    // Ktor Core
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+
+    // Content Negotiation and Serialization
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Logging
+    // implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackclassic_version")
+    implementation("io.github.microutils:kotlin-logging-jvm:$micrologging_version")
+
+    // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
