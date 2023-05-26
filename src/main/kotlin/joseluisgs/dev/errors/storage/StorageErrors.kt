c00d6bb86e550856a3f471b8010c49b7aa60a435
José Luis González <joseluisgonsan@gmail.com>
2023-05-26 20:17:46 +0200

    fixed racquet to racket

--- src/main/kotlin/joseluisgs/dev/errors/storage/StorageErrors.kt
+++ src/main/kotlin/joseluisgs/dev/errors/storage/StorageErrors.kt
@@ -1,4 +1,4 @@
-package joseluisgs.dev.errors.racket
+package joseluisgs.dev.errors.storage
 
 sealed class StorageError(val message: String) {
     class NotFound(message: String) : StorageError(message)
